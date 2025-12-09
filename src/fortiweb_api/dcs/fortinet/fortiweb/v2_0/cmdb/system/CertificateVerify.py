9677ca2dfc1acbc2741ba5d038b18ad026087cf5
Fabian Hubacher <fabian.hubacher@swisscom.com>
2025-12-09 12:30:42 +0100

    Endpoint CertificateVerify

---
+++ src/fortiweb_api/dcs/fortinet/fortiweb/v2_0/cmdb/system/CertificateVerify.py
@@ -0,0 +1,26 @@
+from typing import List, Optional, Dict, Any, ClassVar, Type
+from marshmallow import Schema
+from marshmallow_dataclass import dataclass as mdc
+from dataclasses import field
+
+from fortiweb_api.dcs.EndpointBase import EndpointBase
+from fortiweb_api.dcs.fortinet.fortiweb.BaseSchema import Baseschema
+
+@mdc(base_schema=Baseschema)
+class CertificateVerify(EndpointBase):
+    q_ref: Optional[int]
+    q_ref_string: Optional[str]
+    name: str = field(metadata=EndpointBase.post_mark | EndpointBase.key_mark)
+    ca: str = field(metadata=EndpointBase.post_mark)
+    crl: str = field(metadata=EndpointBase.post_mark)
+    crl_allow_failure: str = field(metadata=EndpointBase.post_mark)
+
+    Schema: ClassVar[Type[Schema]] = Baseschema
+
+    @classmethod
+    def get_post_json_template(cls) -> str:
+        cls.post_required_fields = []
+        return super().get_post_json_template()
+
+
+
