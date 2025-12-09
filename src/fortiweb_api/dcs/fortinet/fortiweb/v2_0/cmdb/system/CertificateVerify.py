6b3b4c23f3d53608cf87b401e0a531bf021ef904
Fabian Hubacher <fabian.hubacher@swisscom.com>
2025-12-09 12:58:22 +0100

    fix CertificateVerify fields

--- src/fortiweb_api/dcs/fortinet/fortiweb/v2_0/cmdb/system/CertificateVerify.py
+++ src/fortiweb_api/dcs/fortinet/fortiweb/v2_0/cmdb/system/CertificateVerify.py
@@ -8,12 +8,17 @@ from fortiweb_api.dcs.fortinet.fortiweb.BaseSchema import Baseschema
 
 @mdc(base_schema=Baseschema)
 class CertificateVerify(EndpointBase):
+    id: Optional[int]
     q_ref: Optional[int]
     q_ref_string: Optional[str]
     name: str = field(metadata=EndpointBase.post_mark | EndpointBase.key_mark)
     ca: str = field(metadata=EndpointBase.post_mark)
     crl: str = field(metadata=EndpointBase.post_mark)
-    crl_allow_failure: str = field(metadata=EndpointBase.post_mark)
+    publish_dn: str = field(metadata=EndpointBase.post_mark)
+    strictly_need_cert: str = field(metadata=EndpointBase.post_mark)
+    partial_chain: str = field(metadata=EndpointBase.post_mark)
+    crl_allow_expired: str = field(metadata=EndpointBase.post_mark)
+
 
     Schema: ClassVar[Type[Schema]] = Baseschema
 
